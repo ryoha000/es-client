f9ec86c6653a64c7dcb9dd021aeac45328785539
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2020-06-01 13:56:30 +0900

    add header

---
+++ src/components/MainView/Header/MainViewHeader.vue
@@ -0,0 +1,40 @@
+<template>
+  <div :class="$style.container">
+    <q-btn flat icon="keyboard_arrow_left"/>
+    <q-btn flat icon="keyboard_arrow_right"/>
+    <q-btn flat icon="home" />
+    <q-input :class="$style.input" rounded outlined v-model="searchString" dark>
+      <template v-slot:prepend>
+        <q-icon name="search" color="white" :class="$style.icon" />
+      </template>
+    </q-input>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'MainViewHeader',
+  props: {
+  },
+  components: {
+  },
+  setup() {
+    const searchString = ref('')
+    return { searchString }
+  }
+});
+</script>
+
+<style lang="scss" module>
+.container {
+  display: flex;
+  height: 36px;
+  margin-left: 8px;
+  margin-top: 8px;
+}
+.input {
+  margin-left: auto;
+}
+</style>
