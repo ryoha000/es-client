666afe84657ddb9a78b366c94bc0d5f80e22e366
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2020-06-02 01:54:39 +0900

    add Link component

---
+++ src/components/MainView/GameDetail/Link.vue
@@ -0,0 +1,53 @@
+<template>
+  <div :class="$style.container" @click="open">
+    <div :class="$style.wrapper">
+      <div :class="$style.title">{{ title }}</div>
+      <q-icon name="open_in_new" size="20px" :class="$style.icon" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'Link',
+  props: {
+    title: { type: String, required: true },
+    url: { type: String, required: true },
+  },
+  setup(props) {
+    const open = () => {
+      window.open(props.url)
+    }
+    return { open }
+  }
+});
+</script>
+
+<style lang="scss" module>
+.container {
+  color: #0366d6;
+  cursor: pointer;
+  height: 28px;
+  padding-bottom: -1px;
+  :hover {
+    color: navy;
+    border-bottom: thin solid navy;
+    .title {
+      border-bottom: none;
+    }
+    .icon {
+      border-bottom: none;
+    }
+  }
+}
+.wrapper {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.title {
+  font-size: 20px;
+}
+</style>
