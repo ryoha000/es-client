217f0d26d9b1155786925366d92ed5d0d0744d96
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2020-06-02 18:58:20 +0900

    add scraping method

---
+++ src/components/use/useScraping.ts
@@ -0,0 +1,40 @@
+import * as Request from 'request-promise-native';
+//import * as Charset from 'chardet';
+//import * as iconv   from 'iconv-lite';
+import { JSDOM }    from 'jsdom';
+
+const baseURL = 'https://erogamescape.dyndns.org/~ap2/ero/toukei_kaiseki'
+
+const useScraping = () => {
+  const getDocument = async (url: string) => {
+    const option = {
+      url: url,
+      encoding: null
+    };
+    const response = await Request.get(option);
+  
+    // 文字コード判別
+    // const encoding = Charset.detect(response)?.toString();
+    // if(!encoding) throw new Error();
+  
+    // 文字コード変換
+    //const html: string = iconv.decode(response, encoding);
+  
+    // HTMLパース
+    const jsdom = new JSDOM(response);
+    return jsdom.window.document;
+  }
+  const getTitle = async () => {
+    const document = await getDocument(baseURL)
+    return document.title
+  }
+  const getGameDetail = async (id: number) => {
+    const url = baseURL + `/game.php?game=${id}`
+    const document = await getDocument(url)
+    const elm = document.querySelector('hogehoge');
+    return elm
+  }
+  return { getTitle, getGameDetail }
+}
+
+export default useScraping
