736d73bd19e3b3e9ffdc4139080d4c56b9f0da56
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2020-06-22 00:55:12 +0900

    refactor

--- src/components/MainView/GameDetail/PlayButton.vue
+++ src/components/MainView/GameDetail/PlayButton.vue
@@ -46,6 +46,7 @@ import * as ChileProcess from 'child_process'
 import * as iconv from 'iconv-lite'
 import * as path from 'path'
 import useJson from '../../use/useJson';
+import useStartProcess from '../../use/useStartProcess'
 
 export default defineComponent({
   name: 'PlayButton',
@@ -63,32 +64,8 @@ export default defineComponent({
   setup(props, context) {
     const { readFileConsoleErr, override, getHistory } = useJson()
     const startProcess = async (isAdmin: boolean | undefined) => {
-      if (isAdmin === undefined) {
-        isAdmin = JSON.parse(await readFileConsoleErr('setting/setting.json')).isVerbRunAs
-      }
-      const listGame = props.gameInList[props.game.id]
-      if (listGame) {
-        const splitPath = listGame.path.split('\\')
-        const exe = splitPath.pop()
-        const fp = splitPath.join('\\')
-        const normalizedFile = path.normalize(path.resolve(fp)).replace(/\'/g,'\'\'')
-        try {
-          await readFileConsoleErr('setting/playtime.json')
-        } catch (e) {
-          await override('setting/playtime.json', '')
-        }
-        const addStartTime = `$date = Get-Date ; Add-Content setting/playtime.json \"start\`n${listGame.id}\`n$date\"`
-        const addFinishTime = `$date = Get-Date ; Add-Content setting/playtime.json \"end\`n${listGame.id}\`n$date\"`
-        const command = `$now = pwd ; ${addStartTime} ; cd \'${normalizedFile}\' ; powershell Start-Process ${exe} ${isAdmin ? '-verb runas' : ''} -Wait ; cd $now.path ; ${addFinishTime}`
-        // 何故か if(Buffer.isBuffer(stderr)) で判定してもダメだからanyに
-        ChileProcess.exec(`powershell.exe -command "${command}"`, {encoding: 'binary', maxBuffer: 64*1024*1024}, (err, stdout, stderr: any) => {
-          if (err) {
-            const str = iconv.decode(stderr, 'shiftjis')
-            alert(str)
-          }
-          return
-        })
-      }
+      const { startProcess } = useStartProcess(props.gameInList[props.game.id])
+      await startProcess(isAdmin)
     }
     const isOpenRelationDialog = ref(false)
     const closeRelationDialog = () => {
---
+++ src/components/use/useStartProcess.ts
@@ -0,0 +1,39 @@
+import useJson from "./useJson"
+import * as ChileProcess from 'child_process'
+import * as iconv from 'iconv-lite'
+import * as path from 'path'
+import { ListGame } from "src/types/root"
+
+const useStartProcess = (game: ListGame) => {
+  const startProcess = async (isAdmin: boolean | undefined) => {
+    const { readFileConsoleErr, override, getHistory } = useJson()
+    if (isAdmin === undefined) {
+      isAdmin = JSON.parse(await readFileConsoleErr('setting/setting.json')).isVerbRunAs
+    }
+    if (game) {
+      const splitPath = game.path.split('\\')
+      const exe = splitPath.pop()
+      const fp = splitPath.join('\\')
+      const normalizedFile = path.normalize(path.resolve(fp)).replace(/\'/g,'\'\'')
+      try {
+        await readFileConsoleErr('setting/playtime.json')
+      } catch (e) {
+        await override('setting/playtime.json', '')
+      }
+      const addStartTime = `$date = Get-Date ; Add-Content setting/playtime.json \"start\`n${game.id}\`n$date\"`
+      const addFinishTime = `$date = Get-Date ; Add-Content setting/playtime.json \"end\`n${game.id}\`n$date\"`
+      const command = `$now = pwd ; ${addStartTime} ; cd \'${normalizedFile}\' ; powershell Start-Process ${exe} ${isAdmin ? '-verb runas' : ''} -Wait ; cd $now.path ; ${addFinishTime}`
+      // 何故か if(Buffer.isBuffer(stderr)) で判定してもダメだからanyに
+      ChileProcess.exec(`powershell.exe -command "${command}"`, {encoding: 'binary', maxBuffer: 64*1024*1024}, (err, stdout, stderr: any) => {
+        if (err) {
+          const str = iconv.decode(stderr, 'shiftjis')
+          alert(str)
+        }
+        return
+      })
+    }
+  }
+  return { startProcess }
+}
+
+export default useStartProcess
