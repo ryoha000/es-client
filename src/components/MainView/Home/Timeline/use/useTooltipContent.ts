babfc86820de3d6d590d7dd96c85a51695eb60e1
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2020-07-26 05:36:11 +0900

    fix tooltip

--- src/components/MainView/Home/Timeline/use/useTooltipContent.ts
+++ src/components/MainView/Home/Timeline/use/useTooltipContent.ts
@@ -6,15 +6,16 @@ const useTooltipContent = (tl: MaskedTimeline) => {
     if (tl.timeline.log_type === LogType.Review) {
       const review = tl.review
       if (!review) return ''
-      if (review) {
-        return `得点: ${review.tokuten ?? review.okazu_tokuten ?? review.before_tokuten ?? 0}\n一言: ${review.hitokoto ?? review.before_hitokoto ?? ''}\n${review.memo ? '長文感想(' + review.memo.length.toString() + ')' : ''}`
+      if (review.tokuten) {
+        return `得点: ${review.tokuten}\n一言: ${review.hitokoto ?? '未登録'}\n${review.memo ? '長文感想(' + review.memo.length.toString() + ')' : ''}`
       }
+      if (review.before_hitokoto) {
+        return `発売前得点: ${review.before_tokuten ?? '未登録'}\n発売前一言: ${review.before_hitokoto}`
+      }
+      return '詳細未登録'
     }
-    if (tl.timeline.log_type === LogType.Play) {
-      const game = tl.game
-      return `中央値: ${game.median ?? 0}\n標準偏差: ${game.stdev ?? 0}\nデータ数: ${game.count2 ?? 0}\n発売日: ${game.sellday ?? '2030-03-31'}`
-    }
-    return ''
+    const game = tl.game
+    return `中央値: ${game.median ?? 0}\n標準偏差: ${game.stdev ?? 0}\nデータ数: ${game.count2 ?? 0}\n発売日: ${game.sellday ?? '未定'}`
   }
   return { makeContent }
 }
